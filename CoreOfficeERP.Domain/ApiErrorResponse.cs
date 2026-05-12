86c756af27c1c798c1bf1a76685e1fcbfc5aa2e4
Rajkumar Shakya <rjkumarshakya1985@gmail.com>
2026-05-12 12:34:22 +0530

    Handle Thow exception

---
+++ CoreOfficeERP.Domain/ApiErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreOfficeERP.Domain
+{
+    public class ApiErrorResponse
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
